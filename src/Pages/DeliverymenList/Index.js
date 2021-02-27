8882a32d17d2e1f60b8137a6a26e56c15ef86e3e
zZubat <leonardomagerote@gmail.com>
2021-02-26 21:07:11 -0300

    função pdf

--- src/Pages/DeliverymenList/Index.js
+++ src/Pages/DeliverymenList/Index.js
@@ -1,6 +1,10 @@
 import React, { useCallback, useEffect, useState} from 'react';
+import { remote } from 'electron';
+import path from 'path';
+import fs from 'fs';
 import GlobalMenu from '../../components/GlobalMenu';
 import api from '../../services/api';
+import { date } from 'yup';
 
 
 const DeliverymenList = () => {
@@ -25,6 +29,40 @@ const DeliverymenList = () => {
         loadingDeliverymen()
     }, [loadingDeliverymen]);
 
+    const ShowNotification = (title, msg) => {
+        const notification = { 
+            title: title,
+            body: msg,
+            icon: path.join(__dirname, '../../assets/logo.png'),
+        }
+        new remote.Notification(notification).show();
+    }
+
+    const savePdf = async () => {
+        const filePath = path.join(__dirname, `../../assets/lista_de_entregadores${new Date().toDateString()}.pdf`);
+        const win = remote.BrowserWindow.getFocusedWindow(); 
+        const options = {
+            marginsType: 0,
+            pageSize: 'A4',
+            printBackground: true,
+            landscape: false,
+        }
+
+        try {
+            const pdf = await win.webContents.printToPDF(options);
+            fs.writeFile(filePath, pdf, (error) => {
+                if (error){
+                    ShowNotification('Error', `erro ao salvar arquivo. ${error}`);
+                } else {
+                    ShowNotification('PDF Exportado', `PDF exportado com sucesso`)
+                    console.log('PDF Exportado com Sucesso.')
+                }
+            });
+        } catch (error) {
+            ShowNotification('Error', `erro ao gerar arquivo. ${error}`);
+        }
+    };
+
     return(
         <>
             <GlobalMenu />
@@ -40,6 +78,15 @@ const DeliverymenList = () => {
                         value={perPage}
                         onChange={(e) => setPerpage(e.target.value)}                        
                         />
+            </div>
+            <div>
+                <button
+                id=''
+                type='button'
+                onClick={() => savePdf()}
+                >
+                    Exportar PDF
+                </button>
             </div>
             <ul>
                 {deliverymen.map((d) => {
